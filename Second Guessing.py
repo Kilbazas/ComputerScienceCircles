d888084ccf84b7a147eb92fbb45596a8fffa8554
Kilbazas <35705748+Kilbazas@users.noreply.github.com>
2020-09-16 00:16:26 +0300

    Create Second Guessing.py

---
+++ Second Guessing.py
@@ -0,0 +1,7 @@
+# goal: print how many seconds is in a week  
+secondsPerMinute = 60
+secondsPerHour = secondsPerMinute * 60 # todo: check!
+secondsPerDay = secondsPerHour * 24
+daysPerWeek = 7
+# daysPerWeek = daysPerWeek + 2 # do i need to count a holiday?
+print(secondsPerDay * daysPerWeek)
