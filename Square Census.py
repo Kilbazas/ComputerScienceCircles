89b4d7e69032d6c81e042c8af171cfe1e4ff09e0
Kilbazas <35705748+Kilbazas@users.noreply.github.com>
2020-09-17 15:46:31 +0300

    Create Square Census.py

---
+++ Square Census.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range (1, n):
+   r = i*i
+   if(r < n):
+      print(r)
+      
+#import math
+#n = int(input())
+#for i in range (1, n):
+ #  number = int(math.sqrt(i))
+  # if number < n:
+   #      print(number*number)
+        
